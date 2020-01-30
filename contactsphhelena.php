66edbd4b94a95a247b405c3e2b2e95d9ce885d93
Dave M <42705084+daveminkowski@users.noreply.github.com>
2020-01-30 13:31:19 -0700

    Concept for Biomet Service Request

---
+++ contactsphhelena.php
@@ -0,0 +1,29 @@
+<?php
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['contact-name'];
+	$department = $_POST['department'];
+	$contactphone = $_POST['contact-phone'];
+	$contactemail = $_POST['contact-email'];
+	
+	$devicetype = $_POST['device-type'];
+	$devicelocation = $_POST['device-location'];
+	$deviceid = $_POST['device-id'];
+
+	$servicedescription = $_POST['device-problem'];
+	
+	$mailTo = "Nicholas.Wagner@ge.com";
+	$subject = "Service Request from SPH Helena.";
+	$message = "From:  ".$name."\n".
+		"Department:  ".$department."\n".
+		"Contact Phone:  ".$contactphone."\n".
+		"Contact Email:  ".$contactemail."\n\n".
+		"Device Type:  ".$devicetype."\n".
+		"Device Control Number:  ".$deviceid."\n".
+		"Device Location:  ".$devicelocation."\n\n".
+		"Description of Problem:  ".$servicedescription;
+	
+	mail($mailTo, $subject, $message);
+	
+	header("Location: index.html?mailsend");
+}
